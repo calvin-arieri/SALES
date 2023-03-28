8181a7f7c3b52875e8b79bd609562c7a07c92c8d
calvin-arieri <calvin.arieri@student.moringaschoool.com>
2023-03-28 11:37:24 +0300

    rendering

--- index.js
+++ index.js
@@ -2,17 +2,17 @@
 const url ="http://localhost:3000/f1Cars"
 fetch(url)
 .then(res => res.json())
-.then(f1Cars => console.log(f1Cars))
+.then(f1Cars => f1Cars.forEach(cars=> displayProducts(cars)))
 
 // Displaying products in html
-/*function displayProducts(cars){
+function displayProducts(cars){
     const products = document.createElement("p")
     products.className = "card"
     products.innerHTML =`
     <img src=${cars.image}><br>
-    <p>${cars.nane}</p><br>
+    <p>${cars.name}</p><br>
     <p>${cars.description}</p><br>
     <p>${cars.likes}</p>
     `
-    document.querySelector("#displayProducts").appendChild(card)*/
-//}
+    document.querySelector("#displayProducts").appendChild(products)
+}
